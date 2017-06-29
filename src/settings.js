5cc71d6fd568bf03184db22a7a1a9164317e0422
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2017-06-28 21:27:15 -0400

    switch to url that includes scoped packages

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
     cachePath: process.env.CACHE_OUTPUT || '/tmp/npm-registry-dump.json',
-    allURL: 'https://skimdb.npmjs.com/registry/_all_docs',
+    allURL: 'https://replicate.npmjs.com/registry/_all_docs', // for unscoped only: 'https://skimdb.npmjs.com/registry/_all_docs'
     keyOutput: process.env.KEY_OUTPUT || '/tmp/npm-all-keys.txt',
 };
